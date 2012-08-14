a5ba10f750a72f6967594ea4a2abe2c5131ee983
inohiro <mammymax@gmail.com>
2012-08-14 16:17:33 +0900

    add etl, rdf_file

---
+++ lib/etl.rb
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+module LDETL
+
+  #
+  # = ETL Class
+  #
+  # データベースへの格納，変換，スキーマ導出をサポートする
+  #
+  class ETL
+
+    def initializer( schema_name, rdf_file, load_type )
+      @schema_name = schema_name
+      @rdf_file = rdf_file
+      @load_type = load_type
+    end
+
+    def run
+      extractor = LDETL::Extractor.new( @schema_name, @rdf_type, @load_type )
+      extractor.extract
+
+#      transformer = LEDTL::Transformer.new( extractor )
+#      transformer.transform
+
+#      loader = LDETL::Loader.new
+#      loader.load
+    end
+
+    #
+    # 
+    def measure_candidates
+    end
+
+    def table_info( table = nil )
+      if table == nil
+        # return all table's information
+      else
+        # return correspond table's information
+      end
+    end
+
+    def relations( table = nil )
+      if table == nil
+        # return all table's relationships
+      else
+        # return correspond table's relationships
+      end
+    end
+
+    def create_schema( measure_candidate )
+    end
+    
+  end
+end
---
+++ lib/rdf_file.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+module LDETL
+
+  #
+  # = RdfFile Class
+  #
+  # RDF file
+  #
+  class RdfFile
+
+    def initializer( file_path, file_type )
+      @file_path = file_path
+      @file_type = file_type
+    end
+
+  end
+end
