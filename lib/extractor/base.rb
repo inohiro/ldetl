63222ed0e00ce91b0a7d8bd7602d6b39a084ff67
inohiro <mammymax@gmail.com>
2012-08-14 16:30:01 +0900

    add Extractor::Base, All, Separated, Divided_All

---
+++ lib/extractor/base.rb
@@ -0,0 +1,12 @@
+module LDETL
+  module Extractor
+    class Base
+      def initialize( etl )
+        @etl = etl
+      end
+
+      def etl_params
+      end
+    end
+  end
+end
---
+++ lib/extractor/separate.rb
@@ -0,0 +1,14 @@
+module LDETL
+  module Extractor
+    class Separated < Base
+      def vertical_extract
+      end
+      
+      def horizontal_extract
+      end
+
+      def duplicate
+      end
+    end
+  end
+end
