e50b989758b02aa8dc3a9f224f8e7d92e29d7468
Hiroyuki Inoue <mammymax@gmail.com>
2012-08-31 12:17:36 +0900

    add run.rb

---
+++ run.rb
@@ -0,0 +1,17 @@
+
+require 'ldetl'
+
+schema_name = 'bill'
+rdf_file = './spec/data/'
+load_type = 'vertical'
+
+etl = LDETL::ETL.new( schema_name, rdf_file, load_type )
+etl.run
+
+measure_candidates = etl.measure_candidates
+measure_candidates.each do |candidate|
+  pp candidate
+end
+
+schema = etl.generate_schema( measure_candidates.first )
+
