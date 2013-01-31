cb1d242be8b1e871c4777b141a51ed2ddbd03ce8
Hiroyuki Inoue <mammymax@gmail.com>
2013-01-31 23:02:40 +0900

    rename extractor/separate.rb to extractor/separated.rb

--- lib/ldetl.rb
+++ lib/ldetl.rb
@@ -9,7 +9,7 @@ require 'sequel'
 require 'ldetl/db'
 require 'ldetl/etl'
 require 'ldetl/extractor/base'
-require 'ldetl/extractor/separate'
+require 'ldetl/extractor/separated'
 require 'ldetl/extractor/all'
 require 'ldetl/extractor/divided_all'
 
