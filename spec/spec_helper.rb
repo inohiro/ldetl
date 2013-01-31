5c2f57608bd2b713c01380705b229a7e044be919
Hiroyuki Inoue <mammymax@gmail.com>
2013-01-31 17:23:55 +0900

    add setup_db definition

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,20 @@
 require 'rspec'
 require 'rspec/autorun'
 require 'ldetl'
+require 'sequel'
 
-RSpec.configure do |conf|
+RSpec.configure do |config|
   include LDETL
+  config.before :all do
+    setup_db
+  end
+end
+
+DEFAULT_SCHEMA = 'ldetl_sample'
+DATA_PATH = File.expand_path( './data/' )
+
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = ''
+
+def setup_db
 end
