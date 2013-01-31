677f39327e53a664d22cf235b6cccf25c3644987
Hiroyuki Inoue <mammymax@gmail.com>
2013-01-31 22:57:01 +0900

    rewrite db initialize, but isn't used

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,18 +3,36 @@ require 'rspec/autorun'
 require 'ldetl'
 require 'sequel'
 
+require 'pp'
+
 RSpec.configure do |config|
   include LDETL
   config.before :all do
-    setup_db
+#    setup_db
   end
 end
 
-DEFAULT_SCHEMA = 'ldetl_sample'
-DATA_PATH = File.expand_path( './data/' )
+TEST_SCHEMA = 'ldetl_sample'
+DATA_PATH = File.expand_path( './spec/data/' )
 
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = ''
+TEST_USER = 'root'
+TEST_PASSWORD = ''
+
+TEST_OPTIONS = {
+  :user => TEST_USER,
+  :password => TEST_PASSWORD
+}
+
+options = []
 
 def setup_db
+  connection = nil
+  begin
+    connection = Sequel.connect( "#{DEFAULT_DB_TYPE}://#{TEST_USER}:#{TEST_PASSWORD}@#{DEFAULT_HOST}/#{TEST_SCHEMA}",
+                                 { :encoding => DEFAULT_ENCODING } )
+    require 'pp'
+    pp connection
+  rescue => exp
+    puts exp
+  end
 end
